a5b78aaf7ac045b4be69b04b8d8cf425dee4e897
roys10 <roysegev10@gmail.com>
2023-03-28 17:08:25 +0300

    nothing

---
+++ main.py
@@ -0,0 +1,8 @@
+from nornir import InitNornir
+from nornir_utils.plugins.tasks import napalm_configure
+from nornir_utils.plugins.functions import print_result
+import napalm
+
+
+#https://www.youtube.com/watch?v=DAAYN75kVfw&ab_channel=RogerPerkin
+
